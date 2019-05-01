e065c9fb90100b25d727ed56e882fca52032b596
Maddy <maddy@msmadcat.com>
2019-05-01 13:32:32 -0400

    ready to publish

--- R/rickroll.R
+++ R/rickroll.R
@@ -1,8 +1,7 @@
 #' Fun video to watch
 #'
+#' @import utils
 #' @export
 bestThingEver <- function() {
   browseURL("https://www.youtube.com/watch?v=oHg5SJYRHA0", browser = getOption("browser"))
 }
-
-bestThingEver()
